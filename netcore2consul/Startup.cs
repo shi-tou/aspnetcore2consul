daef0219bb53c325b392af61abb625ed355acef7
yanglb <yanglb@fxtcn.com>
2018-07-25 18:40:32 +0800

    asp.net core + consul

---
+++ netcore2consul/Startup.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using netcore2consul.Extensions;
+
+namespace netcore2consul
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IApplicationLifetime lifetime)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+
+            ConsulConfig consulConfig = Configuration.GetSection("Consul").Get<ConsulConfig>();
+            consulConfig.ServiceIP = LocalIPAddress;
+            //注册consul
+            app.RegisterConsul(lifetime, consulConfig);
+        }
+
+        /// <summary>
+        /// 本地ip
+        /// </summary>
+        public static string LocalIPAddress
+        {
+            get
+            {
+                UnicastIPAddressInformation mostSuitableIp = null;
+                var networkInterfaces = NetworkInterface.GetAllNetworkInterfaces();
+
+                foreach (var network in networkInterfaces)
+                {
+                    if (network.OperationalStatus != OperationalStatus.Up)
+                        continue;
+                    var properties = network.GetIPProperties();
+                    if (properties.GatewayAddresses.Count == 0)
+                        continue;
+
+                    foreach (var address in properties.UnicastAddresses)
+                    {
+                        if (address.Address.AddressFamily != AddressFamily.InterNetwork)
+                            continue;
+                        if (IPAddress.IsLoopback(address.Address))
+                            continue;
+                        return address.Address.ToString();
+                    }
+                }
+                return mostSuitableIp != null
+                    ? mostSuitableIp.Address.ToString()
+                    : "";
+            }
+        }
+    }
+
+}
---
+++ netcore2consul/consul.bat
@@ -0,0 +1,2 @@
+﻿sc.exe create "Consul" binPath="D:\consul_1.2.1\consul.exe agent -dev"
+sc.exe start "Consul" 
